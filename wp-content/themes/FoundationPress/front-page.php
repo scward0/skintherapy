a07c280bc2368dccb82cc6402ac5741fb5f19e43
Sean Ward <scward0@gmail.com>
2017-02-06 15:48:42 -0700

    begin front-page.php

---
+++ wp-content/themes/FoundationPress/front-page.php
@@ -0,0 +1,30 @@
+<div style="background: url('http://localhost:88/Skintherapy/wp-content/uploads/2017/02/skintherapy_main.png'); height: 100%; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;">
+  <?php get_header(); ?>
+  <div class="headline">
+    <div class="headline-border">
+      <p class="text-center subhead">
+        let the real you
+      </p>
+      <h1 class="text-center">shine through</h1>
+      <p>
+        At Skintherapy, we know that the path to achieving clear skin is just as uique and individial as each and every one you.
+        Our approach treats acne without antibiotics and harsh prescription topicals.
+        Instead, we use professional-grade skin care, natural supplements and lifestyle adjustments to control acne.
+      </p>
+      <div class="text-center"><a class="button" href="http://198.58.102.147/pbl_birth_injury/birth-injuries/">SEE THE RESULTS</a></div>
+    </div>
+  </div>
+</div>
+
+<section class="no-max" id="road-map">
+  <div class="headline headline2">
+    <div class="headline-border">
+      <p class="text-center subhead">
+        our road map to
+      </p>
+      <h1 class="text-center">clear skin</h1>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -14,42 +14,80 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 		<?php wp_head(); ?>
 	</head>
+
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
-
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-	<div class="off-canvas-wrapper">
-		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
-
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="title-bar" data-responsive-toggle="site-navigation">
-			<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
-			<div class="title-bar-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			</div>
-		</div>
 
-		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
-			<div class="top-bar-left">
-				<ul class="menu">
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-				</ul>
-			</div>
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
-
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
+<div id="header" class="fixed transition">
+	<header class="row no-max pad main">
+		<nav id="mainnav" class="mainnav" role="navigation">
+			<ul class="menu">
+				<style media="screen">
+				div.fullscreenmenu ul.nav-menu{
+					margin-top: 100px !important;
+				}
+				.animateddrawer{
+					display: none !important;
+				}
+				div.fullscreenmenu ul.nav-menu li{
+				  border-bottom: none !important;
+				}
+				.navcontent{
+					padding: 1%;
+				}
+				div.fullscreenmenu ul.nav-menu li a{
+					font-size: 110%;
+					text-transform: uppercase;
+					padding: 10px 0 !important;
+				}
+				div.fullscreenmenu ul.nav-menu li a[href="http://198.58.102.147/pbl_birth_injury/practice_area/statutes-of-limitation/"],
+				div.fullscreenmenu ul.nav-menu li a[href="http://198.58.102.147/pbl_birth_injury/practice_area/cerebral-palsy/"],
+				div.fullscreenmenu ul.nav-menu li a[href="http://198.58.102.147/pbl_birth_injury/practice_area/injuries-to-children/"],
+				div.fullscreenmenu ul.nav-menu li a[href="http://www.medicalmalpracticeattorneyinfo.com/"] {
+					font-size: 75% !important;
+					padding: 8px 0 !important;
+				}
+				@media screen and (max-width: 850px){
+					#schedule{
+						display: none;
+					}
+				}
+				@media screen and (max-width: 425px){
+					.desk-logo{
+						width: 75%;
+					}
+					.nav-menu li a{
+						font-size: 80%;
+					}
+					.fa-bars{
+						top: 20px;
+						right: 30px;
+					}
+					div.fullscreenmenu ul.nav-menu li a{
+						font-size: 110%;
+						text-transform: uppercase;
+						padding: 5px 0 !important;
+					}
+				}
+				</style>
+				<li class="home"><a class="desk-logo" href="<?php bloginfo('url'); ?>"><img src="http://localhost:88/Skintherapy/wp-content/uploads/2017/02/logo.png" width="300" style="z-index: 5; opacity: 1;"alt="<?php bloginfo('name'); ?>"/></a></li>
+				<i class="fa fa-bars fa-3x"  onclick="menu1.togglemenu(); return false"></i>
+			</ul>
+			<div class="navi hide">
+				<?php
+					$defaults = array(
+						'container' => false,
+						'theme_location' => 'primary-menu',
+						'menu_class' => 'no-bullet menu sticky cap'
+					);
+					wp_nav_menu($defaults)
+				 ?>
 			</div>
 		</nav>
 	</header>
-
-	<section class="container">
-		<?php do_action( 'foundationpress_after_header' );
+</div>
