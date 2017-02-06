4c723bafc2fae147ea01485711676b3c60b659f4
Sean Ward <scward0@gmail.com>
2017-02-06 16:35:40 -0700

    add steps

--- wp-content/themes/FoundationPress/front-page.php
+++ wp-content/themes/FoundationPress/front-page.php
@@ -11,7 +11,7 @@
         Our approach treats acne without antibiotics and harsh prescription topicals.
         Instead, we use professional-grade skin care, natural supplements and lifestyle adjustments to control acne.
       </p>
-      <div class="text-center"><a class="button" href="http://198.58.102.147/pbl_birth_injury/birth-injuries/">SEE THE RESULTS</a></div>
+      <div class="text-center"><a class="button" href="#">SEE THE RESULTS</a></div>
     </div>
   </div>
 </div>
@@ -23,6 +23,37 @@
         our road map to
       </p>
       <h1 class="text-center">clear skin</h1>
+    </div>
+  </div>
+</section>
+
+<section class="no-max container" id="road-map-steps">
+  <div class="row">
+    <div class="columns small-12 medium-4 large-4">
+      <h4>1. talk</h4>
+      <p>We provide you with the tools and knowledge to identify what is irritating for your skin.</p>
+      <img src="http://localhost:88/Skintherapy/wp-content/uploads/2017/02/talk.png" alt="">
+    </div>
+    <div class="columns small-12 medium-4 large-4">
+      <img src="http://localhost:88/Skintherapy/wp-content/uploads/2017/02/treat.png" alt="">
+      <h4>2. treat</h4>
+      <p>We creat a long-term skin care program just for you, based on your skin and acne type.</p>
+    </div>
+    <div class="columns small-12 medium-4 large-4">
+      <h4>3. clear</h4>
+      <p>We follow up and fine tune your skin care plan to achieve results</p>
+      <img src="http://localhost:88/Skintherapy/wp-content/uploads/2017/02/clear.png" alt="">
+    </div>
+  </div>
+  <div class="row text-center">
+    <button class="button">LEARN MORE</button>
+  </div>
+</section>
+
+<section class="no- max container" id="book">
+  <div class="headline headline3">
+    <div class="headline-border">
+      <h3 class="text-center">Acne Specialists <img src="http://localhost:88/Skintherapy/wp-content/uploads/2017/02/unnamed-file.png" alt="" width="100px"> Skin Care Experts</h3>
     </div>
   </div>
 </section>
