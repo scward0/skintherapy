ead8f54c57871e3fbe38b1262b082ac67d21e713
Sean Ward <scward0@gmail.com>
2017-02-16 15:10:19 -0700

    begin posts

--- wp-content/themes/FoundationPress/home.php
+++ wp-content/themes/FoundationPress/home.php
@@ -10,11 +10,18 @@
     </div>
   </div>
 
-  <div class="row secondary">
-    <div class="content">
-      <?php the_field('content'); ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="row">
+      <div class="content">
+        <img src="<?php the_field('thumbnail'); ?>" alt="">
+        <h1><?php the_title(); ?></h1>
+      </div>
     </div>
-  </div>
+  <?php endwhile; else : ?>
+  	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <?php endif; ?>
+
+
 </div>
 
 <?php get_footer(); ?>
