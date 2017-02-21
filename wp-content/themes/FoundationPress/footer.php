403a19b4c9268ee87a2b2279a6350373adc212fb
Sean Ward <scward0@gmail.com>
2017-02-21 14:22:15 -0700

    mobile footer

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -1,5 +1,5 @@
 <section class="container" id="footer" style="padding: 100px 0;">
-	<div class="row">
+	<div class="row hide-for-small-only">
 		<div class="columns small-12 medium-4 large-4">
 			<p id="left">
 				1817 south main street, #18<br>
@@ -23,6 +23,33 @@
 				 <li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Google.png" alt=""></a></li>
 				 <li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Pinterest.png" alt=""></a></li>
 		 </ul>
+		</div>
+	</div>
+
+	<div class="row show-for-small-only">
+		<div class="columns small-12">
+			<p id="left">
+				1817 south main street, #18<br>
+				Salt Lake City, UT 84115
+			</p>
+			<p id="right">
+				801-800-6602<br>
+				utahacneclinic@hotmail.com
+			</p>
+		</div>
+		<div class="columns small-12 medium-4 large-4 text-center">
+			<img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/logo_footer.png" alt="">
+		</div>
+		<div class="columns small-12">
+			<div class="menu-centered">
+				<ul class="menu">
+					<li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Instagram.png" alt=""></a></li>
+					<li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Facebook.png" alt=""></a></li>
+					 <li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Google.png" alt=""></a></li>
+					 <li><a href="#"><img src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/Pinterest.png" alt=""></a></li>
+			 	</ul>
+			</div>
+			<p>All Contents &copy; <?php echo date("Y"); ?>. All Rights Reserved. </p>
 		</div>
 	</div>
 </section>
