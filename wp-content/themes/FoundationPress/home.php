1037c3b9d341058dc8adb2e0ee2f21764ccc643a
Sean Ward <scward0@gmail.com>
2017-02-21 12:53:21 -0700

    menu

--- wp-content/themes/FoundationPress/home.php
+++ wp-content/themes/FoundationPress/home.php
@@ -14,9 +14,21 @@
   </div>
 
   <div class="row">
-    <div class="text-center">
-      <?php get_search_form(); ?>
+    <div class="columns small-12 medium-6 large-6">
+      <div class="text-center">
+        <?php get_search_form(); ?>
+      </div>
     </div>
+    <div class="columns small-12 medium-6 large-6">
+      <ul class="menu">
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Skin Information</a></li>
+        <li><a href="#">Acne Information</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="columns small-12 medium-6 large-6 text-center">
           <img src="<?php the_field('thumbnail'); ?>" alt="">
