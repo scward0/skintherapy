ebe28d3a5e3f63d8e88d2031365dc9f5020c0b90
Sean Ward <scward0@gmail.com>
2017-02-21 16:50:55 -0700

    archive

--- wp-content/themes/FoundationPress/archive.php
+++ wp-content/themes/FoundationPress/archive.php
@@ -18,14 +18,16 @@
 get_header(); ?>
 
 <div id="page" role="main">
-	<article class="main-content">
 
-		TEST
 	<?php if ( have_posts() ) : ?>
 
 		<?php /* Start the Loop */ ?>
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+			<div class="columns small-12 medium-6 large-6 text-center" style="padding: 75px 50px;">
+				<img src="<?php the_field('thumbnail'); ?>" alt="">
+				<p style="font-size: 18px;"><?php the_title(); ?></p>
+				<a href="<?php the_permalink(); ?>"><button style="top: 0;" class="button small">READ MORE</button></a>
+			</div>
 		<?php endwhile; ?>
 
 		<?php else : ?>
@@ -44,9 +46,6 @@ get_header(); ?>
 				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
 			</nav>
 		<?php endif; ?>
-
-	</article>
-	<?php get_sidebar(); ?>
 
 </div>
 
