b361b202df27343618873c31c657d7bb43451a75
Sean Ward <scward0@gmail.com>
2017-02-24 12:50:35 -0700

    category thumb

--- wp-content/themes/FoundationPress/category-acne-tips.php
+++ wp-content/themes/FoundationPress/category-acne-tips.php
@@ -33,8 +33,8 @@
   <div class="row">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="columns small-12 medium-6 large-6 text-center" style="padding: 75px 50px;">
-          <img src="<?php the_field('thumbnail'); ?>" alt="">
-          <br><span class="category-label">-<?php the_category( ', ' ); ?> -</span><br>
+          <img src="<?php the_field('thumbnail'); ?>" alt="" style="margin-bottom: 10px;">
+          <br><span class="category-label">-<?php the_category( ', ' ); ?>-</span><br>
 
           <p style="font-size: 18px;"><?php the_title(); ?></p>
           <a href="<?php the_permalink(); ?>"><button style="top: 0;" class="button small">READ MORE</button></a>
