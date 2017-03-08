2a4283ee710f9c3823f4eb23206b781481ca11d0
Sean Ward <scward0@gmail.com>
2017-03-08 09:31:12 -0700

    404

--- wp-content/themes/FoundationPress/404.php
+++ wp-content/themes/FoundationPress/404.php
@@ -1,19 +1,15 @@
 <div class="container ">
   <?php get_header(); ?>
-  <img class="hero" src="<?php the_field('hero'); ?>" width="100%" alt="">
+  <img class="hero" src="http://198.58.102.147/skintherapy/wp-content/uploads/2017/02/education.png" width="100%" alt="">
 
   <div class="row secondary">
     <div class="headline secondary-headline">
-      <div class="headline-border">
+      <div class="headline-border text-center">
 				<h1>Oops</h1>
 				<p>The page you are looking for does not exist</p>
+				<a href="<?php bloginfo('url'); ?>"><button class="button small">HOME</button></a>
       </div>
     </div>
-
-		<li><?php _e( 'Click the <a href="javascript:history.back()">Back</a> button', 'foundationpress' ); ?></li>
   </div>
 
 <?php get_footer();
-
-
-#EC008C;
