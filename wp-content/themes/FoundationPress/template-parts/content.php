157b8c0f0bc8fe02ed7e894a5536feae431b0426
Sean Ward <scward0@gmail.com>
2017-03-08 09:53:32 -0700

    archive

--- wp-content/themes/FoundationPress/template-parts/content.php
+++ wp-content/themes/FoundationPress/template-parts/content.php
@@ -12,20 +12,11 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
 	<div class="row">
-		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-		<?php foundationpress_entry_meta(); ?>
-	</div>
-	<style media="screen">
-		.entry-content p{
-			font-size: 16px;
-		}
-	</style>
-	<div class="entry-content">
-		<div class="columns small-12" style="padding: 30px;">
-			<h3 style="font-size: 24px;"><?php the_title(); ?></h3>
-			<p style="font-size: 16px;"><?php echo wp_trim_words( get_the_content(), 40, '...'); ?></p><br><br>
-			<a href="<?php the_permalink(); ?>"><button style="top: 0;" class="button small">READ MORE</button></a>
+		<div class="entry-content">
+			<div class="columns small-12" style="padding: 30px;">
+				<h3 style="font-size: 24px;"><?php the_title(); ?></h3>
+				<p style="font-size: 16px;"><?php echo wp_trim_words( get_the_content(), 40, '...'); ?></p><br><br>
+				<a href="<?php the_permalink(); ?>"><button style="top: 0;" class="button small">READ MORE</button></a>
+			</div>
 		</div>
-	</div>
-	<hr />
 </div>
